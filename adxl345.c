db2c439cff0f108b95f2a8e0b9365c6b376f1be0
Valery <drobotun.mail@gmail.com>
2023-01-23 21:46:45 +0100

    Update reading function
    	- Add proper reading
    	- Add axis selection (kind of)

--- adxl345.c
+++ adxl345.c
@@ -3,6 +3,7 @@
 #include <linux/kernel.h>
 #include <linux/of.h>
 #include <linux/i2c.h>
+#include <linux/fs.h>
 
 #include "adxl345.h"
 
@@ -89,6 +90,41 @@ static int ADXL345_setup(struct i2c_client *client)
     return ret;
 }
 
+static int ADXL345_read_axis(struct i2c_client *client, int16_t* buf)
+{
+    uint8_t reg = ADXL345_DATAX0_REG;
+    uint8_t data[6];
+    struct i2c_msg msg[2] = {
+        {
+            .addr = client->addr,
+            .len = 1,
+            .buf = (uint8_t*) &reg,
+        },
+        {
+            .addr = client->addr,
+            .len = 6,
+            .buf = data,
+            .flags = I2C_M_RD,
+        }
+    };
+
+    int ret = i2c_transfer(client->adapter, msg, 2);
+
+    size_t i;
+    for (i = 0; i < 6; i += 2)
+    {
+        int16_t raw = data[i + 1];
+        raw = raw << 8 | data[i];
+        buf[i / 2] = raw;
+    }
+
+    return ret;
+}
+
+struct file_operations fops = {
+    .read = adxl345_read,
+};
+
 static int ADXL345_probe(struct i2c_client *client,
 const struct i2c_device_id *id)
 {
@@ -114,11 +150,11 @@ const struct i2c_device_id *id)
         return ret;
     }
 
-    printk(KERN_WARNING "ADXL345 setup success\n");
+    printk(KERN_INFO "ADXL345 setup success\n");
 
 
-    struct adxl345_device* dev = (struct adxl345_device*) kmalloc(sizeof(struct adxl345_device), GFP_KERNEL);
-    struct file_operations* file_operations = (struct file_operations*) kmalloc(sizeof(struct file_operations), GFP_KERNEL);
+    struct adxl345_device* dev = kmalloc(sizeof(struct adxl345_device), GFP_KERNEL);
+
     char* name_buf = kasprintf(GFP_KERNEL, "adxl345-%d", dev_count);
 
     if (dev == NULL || name_buf == NULL)
@@ -127,15 +163,23 @@ const struct i2c_device_id *id)
         return -1;
     } 
 
+    printk(KERN_INFO "ADXL345 memory alloc ok\n");
+
     dev->miscdev.minor = MISC_DYNAMIC_MINOR;
     dev->miscdev.name = name_buf;
     dev->miscdev.parent = &(client->dev);
-    //dev->miscdev.fops =;
+    dev->miscdev.fops = &fops;
+    
     i2c_set_clientdata(client, dev);
 
-    misc_register(&(dev->miscdev));
+    printk(KERN_INFO "ADXL345 misc init ok\n");
+
+    int misc = misc_register(&(dev->miscdev));
+
+    printk(KERN_INFO "ADXL345 misc register ok\n");
 
     dev_count += 1;
+    printk(KERN_INFO "ADXL345: init as /dev/%s\n", dev->miscdev.name);
     return 0;
 }
 
@@ -150,6 +194,13 @@ static int ADXL345_remove(struct i2c_client *client)
         printk(KERN_WARNING "ADXL345 is not responding, error %d\n", ret);
         return ret;
     }
+
+    dev_count -= 1;
+    struct adxl345_device* dev = i2c_get_clientdata(client);
+
+    misc_deregister(&(dev->miscdev));
+    //kfree(dev->miscdev.name);
+    //kfree(dev);
 
     printk(KERN_INFO "ADXL345 disconnected\n");
     return 0;
@@ -196,6 +247,37 @@ static struct i2c_driver ADXL345_driver = {
     .probe = ADXL345_probe,
     .remove = ADXL345_remove,
 };
+
+ssize_t adxl345_read(struct file * file, char __user * buf, size_t count, loff_t * f_pos)
+{
+    struct miscdevice* misc = (struct miscdevice*) (file->private_data);
+    struct adxl345_device* dev = container_of(misc, struct adxl345_device, miscdev);
+    struct i2c_client* client = to_i2c_client(dev->miscdev.parent);
+
+    // Basically we ignore offset, since there's no way back
+    int16_t data[3] = {(0)};
+
+    int ret = ADXL345_read_axis(client, data);
+    
+    if (ret < 0)
+    {
+        printk(KERN_WARNING "Cannot read data from device, error %d\n", ret);
+    }
+
+    printk(KERN_INFO "X data: %hi\n", data[0]);
+    printk(KERN_INFO "Y data: %hi\n", data[1]);
+    printk(KERN_INFO "Z data: %hi\n", data[2]);
+
+    adxl_axis_t axis = ADXL345_AXIS_X;
+
+    if (copy_to_user(buf, (uint8_t*) data + (size_t) axis, 2))
+    {
+        return -EFAULT;
+    }
+
+    return 2;
+}
+
 
 module_i2c_driver(ADXL345_driver);
 
--- adxl345.h
+++ adxl345.h
@@ -8,8 +8,21 @@ typedef enum {
     ADXL345_DATA_FORMAT_REG = 0x31,
     ADXL345_FIFO_CTL_REG = 0x38,
     ADXL345_POWER_CTL_REG = 0x2D,
-    ADXL345_INT_ENABLE_REG = 0x2E
+    ADXL345_INT_ENABLE_REG = 0x2E,
+    ADXL345_INT_MAP_REG = 0x2F,
+    ADXL345_DATAX0_REG = 0x32,
+    ADXL345_DATAX1_REG = 0x33,
+    ADXL345_DATAY0_REG = 0x34,
+    ADXL345_DATAY1_REG = 0x35,
+    ADXL345_DATAZ0_REG = 0x36,
+    ADXL345_DATAZ1_REG = 0x37,
 } adxl_reg_t;
+
+typedef enum {
+    ADXL345_AXIS_X = 0,
+    ADXL345_AXIS_Y = 2,
+    ADXL345_AXIS_Z = 4
+} adxl_axis_t;
 
 
 typedef struct {
@@ -17,6 +30,8 @@ typedef struct {
     uint8_t value;
 } adxl_reg_pair_t;
 
-typedef struct adxl345_device {
+struct adxl345_device {
     struct miscdevice miscdev;
 };
+
+ssize_t adxl345_read(struct file * file, char __user * buf, size_t count, loff_t * f_pos);
